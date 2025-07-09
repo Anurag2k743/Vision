9843f32b9a30e49bdbbf9aa0af22a46936a32fb7
Anurag vashisht <vashishtanurag743@gmail.com>
2025-07-09 16:04:00 +0530

    Initial commit

--- src/Components/Formmenu/Mobilemenu.jsx
+++ src/Components/Formmenu/Mobilemenu.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { IoMenu } from "react-icons/io5";
 import { NavLink } from 'react-router-dom';
 
@@ -8,13 +7,6 @@ const mobilemenu = () => {
             <div className='flex justify-between items-center'>
                 <NavLink to="/Dashboard" className="flex items-center gap-2 text-sm">Dashboard</NavLink>
                 <IoMenu fontSize={25} />
-            </div>
-
-            <div>
-                {/* <NavLink to="/Dashboard" className="flex items-center gap-2 text-sm">Dashboard</NavLink> */}
-                {/* <NavLink to="/Profile" className="flex items-center gap-2 text-sm"> <FaUserAlt fontSize={16} />Profile</NavLink>
-                <NavLink to="/Signup" className="flex items-center gap-2 text-sm"> <FaUserCircle fontSize={16} />Sign Up</NavLink>
-                <NavLink to="/Signin" className="flex items-center gap-2 text-sm"> <MdKey fontSize={16} />Sign In</NavLink> */}
             </div>
         </div>
     )
